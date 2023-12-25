be0dbdb5f0b40778e233ea7a42cfe7d884e9b5e8
CA01972006 <CA01972006@st.kawahara.ac.jp>
2023-12-25 10:45:15 +0900

    エンジンの更新

--- k2EngineLow/k2EngineLowPreCompile.h
+++ k2EngineLow/k2EngineLowPreCompile.h
@@ -77,8 +77,11 @@ using namespace Microsoft::WRL;
 
 #include "graphics/GaussianBlur.h"
 
-const UINT FRAME_BUFFER_W = 1600;	// フレームバッファの幅。
-const UINT FRAME_BUFFER_H = 900;	// フレームバッファの高さ。
+//const UINT FRAME_BUFFER_W = 1600;	// フレームバッファの幅。
+//const UINT FRAME_BUFFER_H = 900;	// フレームバッファの高さ。
+
+const UINT FRAME_BUFFER_W = 1920;	// フレームバッファの幅。
+const UINT FRAME_BUFFER_H = 1080;	// フレームバッファの高さ。
 const UINT UI_SPACE_WIDTH = 1920;	// UI空間の幅。
 const UINT UI_SPACE_HEIGHT = 1080;	// UI空間の高さ。
 static const int MAX_BONE = 512;	// ボーンの最大数。
@@ -109,3 +112,17 @@ static const int MAX_BONE = 512;	// ボーンの最大数。
 #include "graphics/effect/EffectEngine.h"
 #include "sound/SoundSource.h"
 #include "graphics/effect/EffectEmitter.h"
+
+#include "graphics/IRender.h"
+#include "graphics/ModelRender.h"
+#include "graphics/SpriteRender.h"
+#include "graphics/FontRender.h"
+#include "graphics/RenderingEngine.h"
+#include "graphics/Geometry.h"
+#include "Camera/CameraCollisionSolver.h"
+#include "Camera/SpringCamera.h"
+#include "physics/PhysicsSphereObject.h"
+#include "physics/PhysicsStaticObjectPos.h"
+
+#include "level2D/Level2DRender.h"
+#include "level2D/MapChip2DRender.h"
